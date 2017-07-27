d7b9ec9476a551dc63ede2a62ba7be02ad131c3c
alexfhc <alexfang@heytz.com>
2017-07-27 18:01:21 +0800

    更新包名

--- src/ios/track.m
+++ src/ios/track.m
@@ -21,22 +21,25 @@
 
 - (void)pluginInitialize {
     AK = @"4MGRWbGSsjq87oIr0yZmpMst1ieP0fHU";
-    mcode = @"com.heytz.yyang";
+    mcode = @"com.heytz.YYang.app";
     NSLog(@"Cordova Heytz Track Plugin");
     [super pluginInitialize];
 }
 
 - (void)startTrack:(CDVInvokedUrlCommand*)command
 {
-    
-    NSString *ID = [command.arguments objectAtIndex:0];
+
+    NSString *ID = [command.arguments objectAtIndex:1];
     serviceID = [ID integerValue];
-    entityName =  [command.arguments objectAtIndex:1];
+    entityName =  [command.arguments objectAtIndex:2];
+    [self.commandDelegate runInBackground:^{
     sop = [[BTKServiceOption alloc] initWithAK:AK mcode:mcode serviceID:serviceID keepAlive:false];
     [[BTKAction sharedInstance] initInfo:sop];
     BTKStartServiceOption *op = [[BTKStartServiceOption alloc] initWithEntityName:entityName];
     [[BTKAction sharedInstance] startService:op delegate:self.appDelegate];
+        sleep(1);
     [[BTKAction sharedInstance] startGather:self.appDelegate];
+    }];
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];;
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
